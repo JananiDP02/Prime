ba5b119e28c48a932e6773d3e6dc91e5831a728f
JANANI.D.P <JANANI.D.P@LAPTOP-CBE99N5R>
2023-07-24 14:44:50 +0530

    Increment and decrement

---
+++ IncDec/src/Incdec/IncDec.java
@@ -0,0 +1,22 @@
+package Incdec;
+
+public class IncDec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int x = 10;
+		System.out.println("a and b value before the operations: "
+		+ a + " " + b);
+		++a;
+		int c = ++a +  a--;
+		System.out.println("c value after the operations: " + c);
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+			break;
+			System.out.println(i);
+	}
+
+}
+}
