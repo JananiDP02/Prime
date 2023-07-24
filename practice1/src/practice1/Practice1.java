f3ae4fcfb64b4cea4dd35e7ea8a604471c452def
JANANI.D.P <JANANI.D.P@LAPTOP-CBE99N5R>
2023-07-24 14:16:19 +0530

    Practice

---
+++ practice1/src/practice1/Practice1.java
@@ -0,0 +1,31 @@
+package practice1;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		char c=66;
+		System.out.println(c);
+		int a='B';
+		System.out.println(a);
+		int value1 = 9 / 2; //integer division
+		float value2 = 101f / 61f;
+		double value3 = 10d / 6d;
+		float value4 = 5/2.5f; //float division
+		System.out.println("value 1=" + value1); 
+		System.out.println("value 2=" + value2);
+		System.out.println("value 3=" + value3);
+		System.out.println("value 4=" + value4);
+		int marker = 512;
+		//assigning expression to variable 
+		double percentage = marker * 0.46f;
+		System.out.println("percentage :" + percentage);
+		boolean flag=false;
+		System.out.println("boolean value is "+flag);
+		char ch = 'A';
+		int i3 = ch;
+		System.out.println(i3);
+		char var1 = 161;
+		//int i4 = var1;
+		System.out.println(var1);
+	}
+
+}
